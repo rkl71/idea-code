c8aaebe43d34913ecfd6d0e8bf169641e012271b
rkl71 <renkelin00@gmail.com>
2025-07-30 15:20:52 +0900

    init: backend initialization

---
+++ src/main/java/com/hanyang/ideacode/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.hanyang.ideacode.controller;
+
+import com.hanyang.ideacode.common.BaseResponse;
+import com.hanyang.ideacode.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
---
+++ src/test/java/com/hanyang/ideacode/IdeaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanyang.ideacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
