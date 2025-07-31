01e7013d7b74973dbc13441cc49789aac65e9f4a
rkl71 <renkelin00@gmail.com>
2025-07-31 16:16:11 +0900

    feat: backend user module completed

--- src/main/java/com/hanyang/ideacode/IdeaCodeApplication.java
+++ src/main/java/com/hanyang/ideacode/IdeaCodeApplication.java
@@ -1,9 +1,11 @@
 package com.hanyang.ideacode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hanyang.ideacode.mapper")
 public class IdeaCodeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hanyang/ideacode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.hanyang.ideacode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.hanyang.ideacode.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://www.renkelin.vip">Kolin</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
